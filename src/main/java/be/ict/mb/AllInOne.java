766434cc3fe3a1e93494c7b7d5645026d0b1e479
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2021-03-14 18:09:28 +0100

    Delegates in Plain Old Java

--- src/main/java/be/ict/mb/AllInOne.java
+++ src/main/java/be/ict/mb/AllInOne.java
@@ -1,4 +1,15 @@
 package be.ict.mb;
 
-public class AllInOne extends Printer, Scanner {
+public class AllInOne {
+
+    private Printer meAsAPrinter = new Printer();
+    private Scanner meAsAScanner = new Scanner();
+
+    public void print(String text) {
+        meAsAPrinter.print(text);
+    }
+
+    public String scan() {
+        return meAsAScanner.scan();
+    }
 }
